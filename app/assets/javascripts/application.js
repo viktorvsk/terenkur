013627ff98d6d6ebef09ffb5a1ab0ce3b4d1b886
victorvsk <victor@vyskrebentsev.ru>
2015-01-18 19:57:19 +0200

    Some design and datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require picker
 //= require_tree .
 
 
@@ -19,6 +20,18 @@ $(document).ready(function(){
 
   $('.message .close').on('click', function() {
     $(this).closest('.message').fadeOut();
+  });
+
+  datepickr('#search-date');
+
+  datepickr.prototype.l10n.months.shorthand = ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'];
+  datepickr.prototype.l10n.months.longhand = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
+  datepickr.prototype.l10n.weekdays.shorthand = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+  datepickr.prototype.l10n.weekdays.longhand = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+
+  datepickr('#search-date', {
+    dateFormat: 'Y-m-d',
+    minDate: new Date().getTime() - 1000 * 60 * 60 * 24
   });
 
   $('.image-adder').click(function(e){
@@ -31,10 +44,14 @@ $(document).ready(function(){
   $('#search-submit').click(function(){
     var city    = $('#city').val(),
         type    = $('#event_type').val(),
+        date    = $('#search-date').val(),
         params  = [];
 
     if( city !== "" && city != null){
       params.push(city);
+    }
+    if( date !== "" && date != null){
+      params.push(date);
     }
     if( type !== "" && type != null){
       params.push(type);
