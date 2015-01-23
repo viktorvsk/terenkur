dad202afd37b0edd577e7c3b7b1b11f4c7d2f6d9
victorvsk <victor@vyskrebentsev.ru>
2015-01-23 18:44:38 +0200

    Remove test, using rspec soon

--- test/models/event_day_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class EventDayTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/event_meta_type_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class EventMetaTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
