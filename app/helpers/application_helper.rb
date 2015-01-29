a87ac78e5d49c64e77c8ddd2b47763e20f69e30f
victorvsk <victor@vyskrebentsev.ru>
2015-01-29 13:20:21 +0200

    Design and popup body

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def display_popup_body(html)
+    if html =~ /\{\{register\}\}/
+      form = render( partial: 'users/register_user_form', locals: { user: User.new} )
+      html.gsub!('{{register}}', form)
+    end
+    html.html_safe
+  end
+
 end
