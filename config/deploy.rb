8cf36d65ec0cf7edccdc1e7faa20e3444253d5a9
victorvsk <victor@vyskrebentsev.ru>
2015-01-29 03:10:13 +0200

    Basic deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :application,       'terenkur-new'
 set :deploy_to,         "/home/#{fetch(:user)}/#{fetch(:application)}"
 # output format
 set :format,            :pretty
-set :log_level,         :info #:debug
+set :log_level,         :info
 
 # Default value for :linked_files is []
 set :linked_files, %w{config/database.yml config/secrets.yml}
@@ -26,7 +26,8 @@ set :assets_roles, [:all]
 # =========================
 # Additional tasks
 # =========================
-set :default_env, { rvm_bin_path: '~/.rvm/bin' }
+# set :default_env, { rvm_bin_path: '~/.rvm/bin' }
+# set :default_env, { path: "~/.rvm/gems/ruby-2.1.5@default_env/bin:~/.rvm/bin:$PATH" }
 
 
 namespace :deploy do
@@ -34,17 +35,16 @@ namespace :deploy do
   desc "Start unicorn"
   task :start do
     on roles(:all) do
-      execute "bundle exec unicorn -c #{fetch(:unicorn_conf)} -E #{fetch(:rails_env)}"
-      #execute "if [ -f #{fetch(:unicorn_pid)} ] && [ -e /proc/$(cat #{fetch(:unicorn_pid)}) ]; then kill -USR2 `cat #{fetch(:unicorn_pid)}`; else cd #{fetch(:deploy_to)}/current && bundle exec unicorn -c #{fetch(:unicorn_conf)} -E #{fetch(:rails_env)} -D; fi"
+      within current_path do
+        execute :bundle, "exec unicorn -c #{fetch(:unicorn_conf)} -E #{fetch(:rails_env)} -D"
+      end
     end
   end
 
   desc "Stop unicorn"
   task :stop do
     on roles(:all) do
-      within release_path do
-        #execute "./unicornd", :stop
-      end
+     execute "if [ -f #{fetch(:unicorn_pid)} ] && [ -e /proc/$(cat #{fetch(:unicorn_pid)}) ]; then kill -QUIT `cat #{fetch(:unicorn_pid)}`; fi"
     end
   end
 
@@ -52,31 +52,11 @@ namespace :deploy do
   task :restart do
     on roles(:all) do
       within release_path do
-        #execute "./unicornd", :restart
+        invoke 'deploy:stop'
+        invoke 'deploy:start'
       end
     end
   end
-
-  # task :restart do
-  #   run
-  # end
-  # task :start do
-  #   run "cd #{current_release} && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
-  # end
-  # task :stop do
-  #   run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
-  # end
-
-  # desc 'Create symlink'
-  # task :symlink do
-  #   on roles(:all) do
-  #     execute "rm -f #{release_path}/config/database.yml"
-  #     execute "rm -f #{release_path}/config/secrets.yml"
-
-  #     execute "ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  #     execute "ln -s #{shared_path}/config/secrets.yml #{release_path}/config/secrets.yml"
-  #   end
-  # end
 
   desc 'Disable Active Admin'
   task :disable_active_admin do
@@ -97,24 +77,9 @@ namespace :deploy do
   after :finishing, :cleanup
   after :finishing, :restart
 
-  # after :updating, :symlink
-
-  # before :setup, :starting
-  # before :setup, :updating
-  # before :setup, :install
-
   before 'deploy:assets:precompile', :disable_active_admin
   after 'deploy:assets:precompile', :enable_active_admin
 
+  after 'deploy:publishing', :restart
+
 end
-
-
-
-# desc 'Restart application'
-# task :restart do
-#   on roles(:app), in: :sequence, wait: 5 do
-#     sudo "restart #{application}"
-#   end
-# end
-
-
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,7 @@ set :rails_env,   'production'
 set :unicorn_pid, File.join(shared_path, 'tmp', 'pids', 'unicorn.pid')
 set :unicorn_conf, File.join(current_path, 'config', 'unicorn', "#{fetch(:rails_env)}.rb")
 
+
 # Extended Server Syntax
 # ======================
 server '54.68.36.220', roles: :all, user: fetch(:user)
