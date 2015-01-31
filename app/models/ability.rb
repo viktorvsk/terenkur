f94e06e66453fdd4cc1cd6ff0ce7788f21608dc9
victorvsk <victor@vyskrebentsev.ru>
2015-01-31 17:55:48 +0200

    No lazy images and authorize comment delete

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,6 +9,9 @@ class Ability
       else
         can :take_part, Event
         can :create_comment, Event
+        can :destroy_comment, Event
+        cannot :destroy, Comment
+        can :destroy, Comment, user_id: user.id
         cannot :take_part, Event, user_id: user.id
         can :manage, Event, user_id: user.id
         can :manage, User, id: user.id
